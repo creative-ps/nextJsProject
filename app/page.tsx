8c7f5c9fe70f679aed4d6cf5e6040bcb7a128281
creativePs <crativity.im@gmail.com>
2025-06-24 21:26:30 +0530

    rewind

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,16 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { lusitana } from '@/app/ui/font';
-import Image from 'next/image';
 
 export default function Page() {
   return (
-    <>
-    <main className="flex min-h-screen flex-col p-6 mt-4">
+    <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
+        {/* <AcmeLogo /> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
-          />
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -31,47 +26,8 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
-          <Image
-          src={"/hero-desktop.png"}
-          width={1000}
-          height={600}
-          className='hidden md:block'
-          alt="Screenshots of the dashboard project showing desktop version"
-          />
-          <Image
-          src={"/hero-mobile.png"}
-          width={560}
-          height={620}
-          className='block md:hidden'
-          alt="Screenshot of dashboard project showing mobile version"
-          />
         </div>
       </div>
     </main>
-    {/* <div className='mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-md md:max-w-2xl mb-16'>
-      <div className='md:flex'>
-        <div className='md:shrink-0'>
-          <img
-          className='h-48 w-full object-cover md:w-48'
-          src='https://images.unsplash.com/photo-1637734433731-621aca1c8cb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=404&q=80'
-          />
-        </div>
-        <div className='p-8'>
-          <div className='text-sm font-semibold tracking-wide text-indigo-500'>Company retreats</div>
-          <a href='' className='mt-2 block text-md leading-5 font-medium text-black hover:underline'>Incredible accomodation for your team.</a>
-          <p className='text-gray-500 text-sm mt-2 leading-6'>
-            Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.
-          </p>
-        </div>
-      </div>
-    </div>
-
-
-    <div className='sm:max-md:flex p-4 m-4 border max-w-[700px] justify-between shadow-xl'>
-      <div className='h-40 rounded-xl w-48 bg-slate-800 shadow-2xl'></div>
-      <div className='h-40 rounded-xl w-48 bg-orange-800'></div>
-      <div className='h-40 rounded-xl w-48 bg-purple-800 shadow-md'></div>
-    </div> */}
-    </>
   );
 }
